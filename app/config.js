a14c8edc1721445577ecf1360947475870cb9d4d
Houguanyu <bigdeal5658@gmail.com>
2024-01-23 13:56:57 +0800

    更新package.json 設定檔案出現問題導致後端開不起來的狀態

--- app/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secret:'jack-jwt-secret',
-    connectionStr: 'mongodb+srv://bigdeal5658:bigdiff5658@jacktest.wdgsbmu.mongodb.net/?retryWrites=true&w=majority'
-  };
-  
--- app/controllers/home.js
+++
@@ -1,7 +0,0 @@
-class HomeCtl {
-  index(ctx) {
-    ctx.body = "這是首頁";
-  }
-}
-
-module.exports = new HomeCtl();
