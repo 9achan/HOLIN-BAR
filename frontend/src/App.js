a4ffb981a7d386d2d75fc79939b57553f3739f07
bonny <bonnieooop935222@gmail.com>
2024-02-12 22:39:05 +0800

    modify ham navbar3.jsx&css漢堡第三版樣式新增

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "./css/signup.css";
 import "./css/style.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
+import Layout2 from "./Layout2";
 
 import HomePages from "./pages/HomePages";
 import Booking from "./pages/Booking";
@@ -13,19 +14,21 @@ import BookingJumpPage from "./pages/BookingJumpPage";
 
 import AboutUs from "./pages/AboutUs";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<HomePages />} />
-          <Route path="/booking" element={<Booking />} />          
+          <Route path="/booking" element={<Booking />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/news" element={<News />} />
           <Route path="/aboutus" element={<AboutUs />} />
+          {/* <Route path="/signup" element={<SignUp />} /> */}
         </Route>
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Layout2 />}>
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
         <Route path="/booking/jumppage" element={<BookingJumpPage />} />
       </Routes>
     </BrowserRouter>
