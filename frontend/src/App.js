3f48dc5f2ae9eb49c342a2c7ad82cd6574408002
Houguanyu <bigdeal5658@gmail.com>
2024-02-15 12:02:09 +0800

    新增 signup 的成功註冊popup css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/signup.css";
+import "./css/popup.css";
 import "./css/style.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
